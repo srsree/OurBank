bf885d463f1a9c23b5750b798b68a8678c2d946c
muthupandian <muthupandian@mahiti.org>
2011-02-09 14:38:55 +0530

    Resolved Functionality bugs in Bank Module

--- application/modules/bank/forms/Bank.php
+++ application/modules/bank/forms/Bank.php
@@ -31,14 +31,15 @@ class Bank_Form_Bank extends Zend_Form
 	//adm form attribute instance
         $formfield = new App_Form_Field ();
         $name = $formfield->field('Text','name','','','mand','Name',true,'','','','','',1,0);
-        $status = $formfield->field('Checkbox','status','','','','Active',false,'','','','','',1,0);
-        $code = $formfield->field('Text','code','','','','Code',true,'','','','','',1,0);
-        $description = $formfield->field('Text','description','','','','Description',true,'','','','','',1,0);
+        $code = $formfield->field('Text','code','','','mand','Code',true,'','','','','',1,0);
+        $description = $formfield->field('Text','description','','','mand','Description',true,'','','','','',1,0);
         //hidden feilds
         $id = $formfield->field('Hidden','id','','','','',false,'','','','','',0,0);
         $createdBy = $formfield->field('Hidden','created_by','','','','',false,'','','','','',0,1);
         $createdDate = $formfield->field('Hidden','created_date','','','','',false,'','','','','',0,date("y/m/d H:i:s"));
+        $status = $formfield->field('Checkbox','status','','','','Active',false,'','','','','',1,0);
+
 	//add element to form
-        $this->addElements(array($name,$status,$code,$description,$id,$createdBy,$createdDate));
+        $this->addElements(array($name,$code,$description,$id,$createdBy,$createdDate,$status));
     }
 }
