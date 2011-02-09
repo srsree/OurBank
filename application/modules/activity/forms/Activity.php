be69fff1af71a7b2c77a935ffbdc153adec37d3b
muthupandian <muthupandian@mahiti.org>
2011-02-09 13:42:50 +0530

    Resolved css bugs in delete page [ Activities Module]

--- application/modules/activity/forms/Activity.php
+++ application/modules/activity/forms/Activity.php
@@ -26,11 +26,11 @@ class Activity_Form_Activity extends Zend_Form {
                 // create instance for common form field
 		$formfield = new App_Form_Field ();
                 // send parameters to display respective form fields (first parameter is a needed form field)
-		$name = $formfield->field('Text','name','','','mand','Name',true,'','','','','',1,'');
+		$name = $formfield->field('Text','name','','','mand','Name:',true,'','','','','',1,'');
                 $name->addValidator('Alpha', true, array('allowWhiteSpace' => true));
-		$description = $formfield->field('Textarea','description','','','mand','Description',true,'','','',3,18,1,0);
-		$sector_id = $formfield->field('Select','sector_id','','','mand','Sector',true,'','','','','',1,'');
-		$status = $formfield->field('Checkbox','status','','','mand','Active',false,'','','','','',1,'');
+		$description = $formfield->field('Textarea','description','','','mand','Description:',true,'','','',3,18,1,0);
+		$sector_id = $formfield->field('Select','sector_id','','','mand','Sector:',true,'','','','','',1,'');
+		$status = $formfield->field('Checkbox','status','','','mand','Active:',false,'','','','','',1,'');
                 $createdBy = $formfield->field('Hidden','created_by','','','','',"","","","","",false,0,$createdById);
 		$createdDate = $formfield->field('Hidden','created_date','','','','',"","","","","",false,0,date("y/m/d H:i:s"));
 		$this->addElements(array($name,$description,$sector_id,$status,$createdBy,$createdDate));
